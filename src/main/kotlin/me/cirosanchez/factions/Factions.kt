2a1de50afe623f806f836c35b8e1d5d9fbf86cc3
survivalplus-ib <ciroosanchez@outlook.com>
2024-11-26 12:37:34 -0500

    changed stuff to make call legible

--- src/main/kotlin/me/cirosanchez/factions/Factions.kt
+++ src/main/kotlin/me/cirosanchez/factions/Factions.kt
@@ -1,14 +1,12 @@
 package me.cirosanchez.factions
 
-import com.sun.org.apache.xml.internal.serializer.utils.Utils.messages
 import me.cirosanchez.clib.CLib.Companion.plugin
 import me.cirosanchez.clib.adapter.Adapter
 import me.cirosanchez.clib.adapter.impl.DurationTypeAdapter
+import me.cirosanchez.clib.adapter.impl.LocationAdapter
+import me.cirosanchez.clib.adapter.impl.WorldAdapter
 import me.cirosanchez.clib.cLib
 import me.cirosanchez.clib.exception.InvalidConfigurationException
-import org.bukkit.plugin.java.JavaPlugin
-import revxrsal.commands.bukkit.BukkitCommandHandler
-
 import me.cirosanchez.factions.command.SpawnCommand
 import me.cirosanchez.factions.listener.PlayerListener
 import me.cirosanchez.factions.model.configuration.ConfigurationManager
@@ -17,9 +15,9 @@ import me.cirosanchez.factions.model.spawn.SpawnManager
 import me.cirosanchez.factions.model.storage.StorageManager
 import me.cirosanchez.factions.model.user.UserManager
 import me.cirosanchez.factions.model.world.WorldManager
-import me.cirosanchez.factions.util.LocationAdapter
-import me.cirosanchez.factions.util.WorldAdapter
 import org.bukkit.Bukkit
+import org.bukkit.plugin.java.JavaPlugin
+import revxrsal.commands.bukkit.BukkitCommandHandler
 
 
 class Factions : JavaPlugin() {
@@ -42,29 +40,22 @@ class Factions : JavaPlugin() {
     override fun onEnable() {
 
         // Initializing
-        loadCLib()
+        loadAllManagers()
         commandHandler = BukkitCommandHandler.create(this)
-
-        // Loading
-        worldManager.load()
-        regionManager.load()
-        storageManager.load()
-        spawnManager.load()
-        userManager.load()
         commandHandler.register(SpawnCommand())
 
         // Listeners
         server.pluginManager.registerEvents(PlayerListener(), this)
-
     }
 
     override fun onDisable() {
         regionManager.unload()
+        spawnManager.unload()
         userManager.unload()
     }
 
 
-    fun loadCLib(){
+    fun loadAllManagers(){
         plugin = get()
 
         configurationManager.load()
@@ -88,9 +79,15 @@ class Factions : JavaPlugin() {
             messages = true
             mongoURI = uri
             mongoDB = dbName
-            adapters = mutableListOf(me.cirosanchez.clib.adapter.impl.LocationAdapter, me.cirosanchez.clib.adapter.impl.WorldAdapter,
+            adapters = mutableListOf(LocationAdapter, WorldAdapter,
                 DurationTypeAdapter()) as MutableList<Adapter<Any>>
         }
+
+        worldManager.load()
+        regionManager.load()
+        storageManager.load()
+        spawnManager.load()
+        userManager.load()
     }
 
 }
